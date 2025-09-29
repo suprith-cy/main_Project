d592f08668aa93603758662575d02ba00f212dc9
HemanthN-02 <hemanthradhanuppar@gmail.com>
2025-09-29 16:14:34 +0530

    Fixed Api Directing with protectRouting

--- fitverse-frontend/src/App.jsx
+++ fitverse-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import Trainer from "./pages/Trainer";
 import Member from "./pages/Member";
-
+import ProtectedRoute from "./components/protectRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -11,13 +11,34 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/trainer" element={<Trainer />} />
-        <Route path="/member" element={<Member />} />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trainer"
+          element={
+            <ProtectedRoute role="trainer">
+              <Trainer />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/member"
+          element={
+            <ProtectedRoute role="member">
+              <Member />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
